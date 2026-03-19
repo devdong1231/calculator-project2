63eab431adcd5df9cc1872c9a6bb5488ae745c3e
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-03-19 12:05:22 +0900

    STEP 2. - Calculator 클래스 구현(사칙연산 수행 후 결과값 반환, 연간 결과를 저장하는 컬렉션 타입 필드 추가)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,30 @@
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Calculator {
+    private List<Integer> results;
+    private int num1, num2;
+    private char op;
+
+    Calculator(int num1, int num2, char op){
+        results = new ArrayList<>();
+        this.num1 = num1;
+        this.num2 = num2;
+        this.op = op;
+    }
+
+    public int calculate(int num1, int num2, char op){
+        switch(op){
+            case '+': results.add(num1 + num2); break;
+            case '-': results.add(num1 - num2); break;
+            case '*': results.add(num1 * num2); break;
+            case '/':
+                if(num2 == 0)
+                    return 0;
+                results.add(num1 / num2); break;
+            default: return 0;
+        }
+        return results.getLast();
+    }
 
 }
