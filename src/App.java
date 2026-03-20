ce6dfee08e5e354733d0257e2be4b71c205d7e5e
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-03-20 17:23:45 +0900

    fix. 입력부분 예외 처리 완료.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -7,33 +8,67 @@ public class App {
         ArithmeticCalculator<Number> calculator = new ArithmeticCalculator<>();
 
         while (true) {
-            int choice;
-            System.out.println("1. 계산하기\n2. 조회하기");
+            // ---------- 메뉴 구현 ----------
+            System.out.println("1. 계산하기\n2. 조회하기\n계산기를 종료하시려면 \"exit\"를 입력해주세요");
             System.out.print("원하시는 메뉴를 선택해주세요 : ");
-            choice = sc.nextInt();
+            String choice = sc.next();
+            // ------------------------------
 
-            if(choice == 1){
+
+            // 1. ---------- 계산하기 ----------
+            if (choice.equals("1")) {
                 // input1 입력
-                System.out.print("첫 번째 정수를 입력해주세요: ");
-                String input1 = sc.next();
-                // 실수/정수 판별하기
-                if (input1.contains("."))
-                    calculator.setNum1(Double.parseDouble(input1));
-                else
-                    calculator.setNum1(Integer.parseInt(input1));
+                while (true) {
+                    System.out.print("첫 번째 정수를 입력해주세요: ");
+                    String input1 = sc.next();
+                    try { // 숫자가 아닐 경우 예외 처리
+                        // 실수/정수 판별하기
+                        if (input1.contains(".")) {
+                            calculator.setNum1(Double.parseDouble(input1));
+                        } else {
+                            calculator.setNum1(Integer.parseInt(input1));
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("숫자만 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                }
+
 
                 // input2 입력
-                System.out.print("두 번째 정수를 입력해주세요: ");
-                String input2 = sc.next();
-                // 실수/정수 판별하기
-                if (input2.contains("."))
-                    calculator.setNum2(Double.parseDouble(input2));
-                else
-                    calculator.setNum2(Integer.parseInt(input2));
+                while (true) {
+                    System.out.print("두 번째 정수를 입력해주세요: ");
+                    String input2 = sc.next();
+                    try { // 숫자가 아닐 경우 예외 처리
+                        // 실수/정수 판별하기
+                        if (input2.contains("."))
+                            calculator.setNum2(Double.parseDouble(input2));
+                        else
+                            calculator.setNum2(Integer.parseInt(input2));
+                    } catch (NumberFormatException e) {
+                        System.out.println("숫자만 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                }
+
 
                 // 사칙연산 기호 입력
-                System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
-                calculator.setOp(sc.next().charAt(0));
+                while (true) {
+                    System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+                    char op = sc.next().charAt(0);
+                    try { // 사칙 연산 기호가 아닐 경우 예외 발생
+                        if (!(op == '+' || op == '-' || op == '*' || op == '/')) {
+                            throw new InputMismatchException("사칙연산 기호를 입력해주세요");
+                        }
+                        calculator.setOp(op);
+                    } catch (InputMismatchException e) {
+                        System.out.println(e.getMessage());
+                        continue;
+                    }
+                    break;
+                }
 
                 // 최종 결과 계산
                 calculator.calculate();
@@ -45,11 +80,33 @@ public class App {
                 if (exit.equals("exit"))
                     break;
             }
+            // -------------------------------
 
-            else if(choice == 2){
-                System.out.print("입력하신 값보다 큰 결과를 출력합니다. 값을 입력해주세요 :");
-                calculator.getBiggerResults(sc.nextDouble());
+            // 2. ---------- 조회하기 ----------
+            else if (choice.equals("2")) {
+                while (true) {
+                    try {
+                        System.out.print("입력하신 값보다 큰 결과를 출력합니다. 값을 입력해주세요 :");
+                        calculator.getBiggerResults(sc.nextDouble());
+                        break;
+                    } catch (NumberFormatException e) {
+                        System.out.println("숫자를 입력해주세요.");
+                    }
+                }
+
             }
+            // --------------------------------
+
+            else if(choice.equals("exit")){
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
+            // 4. ---------- 메뉴 입력 예외 처리 ----------
+            else {
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+            }
+            // --------------------------------
 
         }
         sc.close();
